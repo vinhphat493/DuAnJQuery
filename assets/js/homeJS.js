72f13b063130450151b95cb406d213307afa5c19
unknown <vinhphat493@gmail.com>
2018-09-24 22:23:13 +0700

    upload lan 2

---
+++ assets/js/homeJS.js
@@ -0,0 +1,81 @@
+// back to top JS
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+$('#myBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+// back to top JS
+
+//navbar
+$(document).scroll(function(){
+    console.log($("#header"))
+    if($(this).scrollTop() > 100 ){
+        $("#header").addClass("fixed-top");
+        $("#header").addClass("scroll-fixed");
+    }else
+    {
+        $("#header").removeClass("fixed-top");
+        $("#header").removeClass("scroll-fixed");
+    }
+});
+//navbar
+
+//form đăng ký
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
+
+  $("#close-modal").click(function(){
+      $(this).addClass(".close");
+  })
+//form đăng ký
+
+
